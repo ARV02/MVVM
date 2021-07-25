feddb5b17ab07199f753ad4d7428cbd361f1f4d0
Andres Rosas <56147310+Chaips@users.noreply.github.com>
2021-07-24 20:09:08 -0500

    bug fix

--- app/src/main/java/com/example/mvvm/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/example/mvvm/network/NetworkConnectionInterceptor.kt
@@ -8,34 +8,35 @@ import com.example.mvvm.utils.NoInternetException
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class NetworkConnectionInterceptor(context:Context) : Interceptor{
+class NetworkConnectionInterceptor(context:Context):Interceptor {
     private val applicationContext = context.applicationContext
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        if (!isInternetAvailable())
-            throw NoInternetException("Verify your Internet connection")
+        if(!isInternetAvailable())
+            throw NoInternetException("No hay connexion a Internet")
         return chain.proceed(chain.request())
     }
 
     @Suppress("DEPRECATION")
-    private fun isInternetAvailable():Boolean{
+    private fun isInternetAvailable() : Boolean {
+        var connected = false
         val connectivityManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE)
-        as ConnectivityManager
+                as ConnectivityManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val nw      = connectivityManager.activeNetwork ?: return false
-            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val activeNetwork =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+
             return when {
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                //for other device how are able to connect with Ethernet
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                //for check internet over Bluetooth
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ||
+                        activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
                 else -> false
             }
         } else {
-            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
-            return nwInfo.isConnected
+            return connectivityManager.activeNetworkInfo != null &&
+                    connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting
         }
     }
 }
